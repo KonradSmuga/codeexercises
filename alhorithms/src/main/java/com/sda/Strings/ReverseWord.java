e65dd819bece021ef134e758ffcf25049f967c2e
KonradSmuga <smuggk@gmail.com>
2019-07-11 15:36:56 +0200

    operation on Strings

--- alhorithms/src/main/java/com/sda/Strings/ReverseWord.java
+++ alhorithms/src/main/java/com/sda/Strings/ReverseWord.java
@@ -6,10 +6,24 @@ public class ReverseWord {
     public String reverse(String word) {
 
 
-
-return new StringBuilder(word).reverse().toString();
+        return new StringBuilder(word).reverse().toString();
     }
 
+
+
+    //przypisanie do znaków, potem wykorzystanie pętli od tyłu i przypisanie znaków do stringbuilder.
+    public String reverseWithIf(String word){
+
+        StringBuilder stringBuilder = new StringBuilder();
+        char[]chars =  word.toCharArray();
+
+        for(int i = chars.length; i >= 0; i --){
+            stringBuilder.append(chars[i]);
+        }
+
+
+        return stringBuilder.toString();
+    }
 
 }
 
