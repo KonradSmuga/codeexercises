f4cdcc29857da7458ec1f1b7390aa647b4672b79
KonradSmuga <smuggk@gmail.com>
2019-09-04 14:47:49 +0200

    show files in catalog

---
+++ inputoutput/src/main/java/Ex9s39.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Ex9s39 {
+    public static void main(String[] args) {
+
+//        System.out.println(getFilesInDirectory(Paths.get("src")));
+//
+//    }
+//
+//    public static List<String> getFilesInDirectory(Path p) {
+//        try {
+//            return Files.walk(p)
+//                    .filter(e -> !Files.isDirectory(e))
+//                    .map(e -> e.getFileName().toString())
+//                    .collect(Collectors.toList());
+//
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//            return List.of();
+//        }
+    }
+}
